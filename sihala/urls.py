7d98bbf9ec732c86778d5d1195353fce20d3d996
Sandarubro95 <sandarubro95@gmail.com>
2025-04-03 00:47:09 +0530

    Main Project Changes

---
+++ sihala/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path, include
+
+from . import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+
+    path('', views.ToSearchRedirectView.as_view()),
+
+    path('search/', include('search.urls')),
+    path('accounts/', include('accounts.urls')),
+]
